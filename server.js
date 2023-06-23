a2578cfa4b5aa4248712c6a68dc5ee79fcd1412e
saideep1000 <110446193+saideep1000@users.noreply.github.com>
2023-06-23 08:40:46 -0500

    Update server.js23

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const PORT= process.env.PORT || 5000;
 
 // Connect to DB and start server
 
-mongoose.connect(process.env.MONGO_URI).then(()=>{
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true }).then(()=>{
     app.listen(PORT,()=>{
         console.log(`Server Running on port ${PORT}`)
     })
